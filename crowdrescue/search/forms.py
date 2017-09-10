7e1d43448a23085a048ee18affdfbdbf46219bf5
Sara Powell <newtcobell@gmail.com>
2017-09-10 16:46:49 -0400

    Stubbed in code to handle incoming JSON, modified template variable names to match twitter's API output

--- crowdrescue/search/forms.py
+++ crowdrescue/search/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from django.utils import timezone
+import requests
+import json
 
 
 class SearchForm(forms.Form):
@@ -7,6 +9,10 @@ class SearchForm(forms.Form):
 
     def search(self):
         # TODO add hook to run the actual search
+        # tweets = requests.get("http://www.example.com")  # will be replaced by twitterbot API address
+        # response = json.loads(tweets)  # used if we need to deserialize the response
+        # return response --> this will send the dictionary over to the template
+
         return [{
             "date": timezone.now(),
             "handle": "@someone",
