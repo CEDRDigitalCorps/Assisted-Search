a2c847045f6ce357e7a954b41ad16027019302a0
Sara Powell <newtcobell@gmail.com>
2017-09-10 22:21:56 -0400

    Search page now password-protected

--- crowdrescue/search/views.py
+++ crowdrescue/search/views.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.template.response import TemplateResponse
 from django.template.loader import render_to_string
@@ -10,11 +11,14 @@ from django.views.generic import FormView, TemplateView
 from crowdrescue.search.forms import SearchForm
 
 
+decorators = [login_required(), csrf_exempt]
+
+
 class HomeView(TemplateView):
     template_name = "home.html"
 
 
-@method_decorator(csrf_exempt, name="dispatch")
+@method_decorator(decorators, name="dispatch")
 class SearchView(FormView):
     template_name = "home.html"
     form_class = SearchForm
@@ -32,3 +36,15 @@ class SearchView(FormView):
             self.template_name,
             context
         ).render()
+
+
+def login_view(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return HttpResponseRedirect('dashboard')
+    else:
+        form = LoginForm()
+    return render(request, 'login.html', {'form': form})
