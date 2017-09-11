da33789b4bb397ea40015200075f7cfa9aad9137
Greg Reinbach <greg@reinbach.com>
2017-09-11 00:20:05 -0400

    Add query type to search form

--- crowdrescue/search/forms.py
+++ crowdrescue/search/forms.py
@@ -1,22 +1,32 @@
 from django import forms
-from django.contrib.auth import authenticate
 from django.utils import timezone
-import requests
-import json
 
 
 class SearchForm(forms.Form):
     query = forms.CharField(max_length=100)
+    query_type = forms.ChoiceField(
+        choices=[
+            ("filter", "Filter"),
+            ("bayesian", "Bayesian"),
+        ],
+        initial="filter"
+    )
 
     def search(self):
         # TODO add hook to run the actual search
         # tweets = requests.get("http://www.example.com")  # will be replaced by twitterbot API address
         # response = json.loads(tweets)  # used if we need to deserialize the response
         # return response --> this will send the dictionary over to the template
+        if self.cleaned_data["query_type"] == "bayesian":
+            # TODO run bayesian search
+            name = "@bayesian"
+        else:
+            # TODO run bayesian search
+            name = "@filter"
 
         return [{
             "created_at": timezone.now(),
-            "screen_name": "@someone",
+            "screen_name": name,
             "text": "random test text",
             "source": "http://example.com",
         }]
