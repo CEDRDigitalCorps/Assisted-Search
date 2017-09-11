e4810838c539a44b107a7ab29c1f0fc956c63331
Greg Reinbach <greg@reinbach.com>
2017-09-11 08:17:31 -0400

    Update search ajax to be asynchronous

--- crowdrescue/static/js/search.js
+++ crowdrescue/static/js/search.js
@@ -1,13 +1,20 @@
 $(function() {
     var url = $("#search-form").attr("action");
+
+    function reqListener() {
+        $("#results").html(this.responseText);
+    }
+
     $("#query").on("keyup", function() {
         $("#results").html('<div id="spinner"><i class="fa fa-spinner fa-spin fa-2x" aria-hidden="true"></i></div>');
-        $.ajax({
-            url: url,
-            method: "POST",
-            data: $("#search-form").serialize()
-        }).done(function(data) {
-            $("#results").html(data);
-        });
+        var req = new XMLHttpRequest();
+        req.addEventListener("load", reqListener);
+        req.open("POST", url);
+        req.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+        req.setRequestHeader(
+            'Content-Type',
+            'application/x-www-form-urlencoded'
+        );
+        req.send($("#search-form").serialize());
     });
 });
